86e2edd78e72082ccda05110b9df70f174fc7f48
Raj Gohil <80092049+rajgohil28@users.noreply.github.com>
2022-11-27 11:46:21 +0530

    Second Commit

--- Assets/Scripts/Config/GameConfig.cs
+++ Assets/Scripts/Config/GameConfig.cs
@@ -3,7 +3,7 @@ namespace Config
 {
     public static class GameConfig
     {
-        public static float SMOOTHNESS = 0.01f;
+        public static float SMOOTHNESS = 0.05f;
         public static float MACHINE_SPEED = 2f;
     }
 }
